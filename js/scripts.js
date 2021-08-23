358d3a6f4b117d8f06fd1841ee41059609c9c354
Marilibojorquez <88338816+Marilibojorquez@users.noreply.github.com>
2021-08-23 01:25:02 -0700

    add updated scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,45 @@
 // Business Logic
 
-function beepBoop(text) {
+function beepBoop(input) {
   let inputArray = []
-    for (var i = 0; i <= InputDeviceInfo; i++) {
-      inputArray.push(i);
+  for (var i = 0; i <= input; i++) {
+    inputArray.push(i);
+  }
+  let countArray = inputArray.map(string);
+  let finalArray = inputArray.mao(string);
+  counterArray.map(function (element) {
+    for ( i = 0; i <= element.length; i++) {
+      if (element[i] === "3") {
+        finalArray[element]
+        finalArray[element] = "We can be best neighboors?";
+      }
+      else if (element[i] === "1" && !(/[2]/g.test(element)) && !(/[3]/g.test(element))) {
+        finalArray[element] = "Beep!";
+      }
+      else if (element[i] === "2" && !(/[3]/g.test(element))) {
+        finalArray[element]
+        finalArray[element] = "Boop!";
+      }
+      else if (element[i] === "2" && (/[2]/g.test(element)) && !(/[3]/g.test(element))) {
+        finalArray[element]
+        finalArray[element] = "Boop!";
+      }
+      else { ' ' }
+      finalArray[element]
     }
-    let countArray = inputArray.map(string);
-    let finaalArray = inputArray.mao(string);
-    counterArray.map(function(element) {
-      for (var i =0; i <= element.length; i++) {
-        if (element[i] === "2") {
-          finaalArray[element] = "We can be best neighboors?";
-        }
-        else if (element[i] === "1" && ! (/[2]/g.test(element)) && !(/[3]/g.test(element))) {
-          finaalArray[element] = "Beep!";
-        }
-        else if (element[i] === "1" && !(/[3]/g.test(element))) {
-          finaalArray[element] = "Boop!";
-        }
-        else if (element[i] === "2" && !(/[2]/g.test)(element))) {
-          ffinaalArray[element] = "Boop!";
-        }
-        else if (element[i] === "1" && !(/[3]/g.test)(element))) {
-          ffinaalArray[element] = "Boop!";
+  });
 
-        }
+  return finalArray;
+}
+
+
+//UI LOGIC
+$(document).ready(function () {
+  $("#submit-form").submit(function (event) {
+    $("#out").empty();
+    $("#out").text("Hi there" + $("input#words").val().charAt(0).toUpperCase() + $("input#words").val().slice(1) + "Can you see what's happening?" + beepBoop($("input#number").val()));
+    $('#inpput-number').val('');
+    event.preventDefault();
+  });
+});
 
