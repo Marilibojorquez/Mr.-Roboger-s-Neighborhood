481ac53de56ed82c945313f0751d34a220934e01
Marilibojorquez <88338816+Marilibojorquez@users.noreply.github.com>
2021-08-26 08:59:15 -0700

    Add updated scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,9 @@
 
 function beepBoop(input) {
   let inputArray = []
-  for (var i = 0; i <= input; i++) {
-    inputArray.push(i);
+  for (let i = 0; i <= input; 0++) {
+    inputArray.push;
+    inputArray.map,["Beep", "Boop"]
   }
   let countArray = inputArray.map(string);
   let finalArray = inputArray.mao(string);
@@ -13,19 +14,21 @@ function beepBoop(input) {
         finalArray[element]
         finalArray[element] = "We can be best neighboors?";
       }
-      else if (element[i] === "1" && !(/[2]/g.test(element)) && !(/[3]/g.test(element))) {
+      else if (element === "1" && !test(element(element))) {
         finalArray[element] = "Beep!";
       }
-      else if (element[i] === "2" && !(/[3]/g.test(element))) {
+      else if (element === "2" && !(test(element))) {
         finalArray[element]
         finalArray[element] = "Boop!";
       }
-      else if (element[i] === "2" && (/[2]/g.test(element)) && !(/[3]/g.test(element))) {
+      else if (element === "2" && !(test(element)(element))) {
         finalArray[element]
         finalArray[element] = "Boop!";
       }
-      else { ' ' }
-      finalArray[element]
+      else if (element === "1" && !(test(element)(element))) {
+        finalArray[element]
+        finalArray[element] = "Boop!";
+      }
     }
   });
 
@@ -36,8 +39,7 @@ function beepBoop(input) {
 //UI LOGIC
 $(document).ready(function () {
   $("#submit-form").submit(function (event) {
-    $("#out").empty();
-    $("#out").text("Hi there" + $("input#words").val().charAt(0).toUpperCase() + $("input#words").val().slice(1) + "Can you see what's happening?" + beepBoop($("input#number").val()));
+    $("#out").text("Hi there Neighboor" + $("input#words").val().charAt(0) + $("input#words").val() + "Wont you be my Neighbor?" + beepBoop($("input#number").val()));
     $('#inpput-number').val('');
     event.preventDefault();
   });
